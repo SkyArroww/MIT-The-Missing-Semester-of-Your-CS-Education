e97602f6cde5e55ba6d941dca1e7b16f85fb039e
SkyArroww <1536513344@qq.com>
2024-03-26 19:57:46 +0800

    Correct new errs

--- Hw2/T4/run.sh
+++ Hw2/T4/run.sh
@@ -3,4 +3,4 @@
 # -type f: regular file
 # -name "*.html": file name ends with .html
 # xargs -d '\n': use newline as delimiter， 分隔find的结果
-find . -type f -name "*.html" | xargs -d '\n' tar -cvzf html.zip
+# find . -type f -name "*.html" | xargs -d '\n' tar -cvzf html.zip
